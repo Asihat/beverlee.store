02605a6810eb94807eea819c9830dd952755238e
mukhan <mr@indigo24.com>
2020-01-14 19:16:20 +0600

    test

--- app/Http/Controllers/API/CheckDataController.php
+++ app/Http/Controllers/API/CheckDataController.php
@@ -5,22 +5,71 @@ namespace App\Http\Controllers\API;
 use App\Goods;
 use App\Http\Controllers\Controller;
 use App\Packet;
+use App\Payments;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CheckDataController extends Controller
 {
-    public function check() {
+    public function check(Request $request) {
+//        return "success";
+//         return $request->packet_id;
+
         $packet_id_array = [1];
         $goods = Goods::all();
+        $total_products = [];
+
         foreach ($packet_id_array as $packet_id){
             if(Packet::find($packet_id)){
-
-                return "Packet found\n" . $goods;
+                $products = DB::table('packet')
+                    -> join('packet_product', 'packet.id', '=', 'packet_product.packet_id')
+                    -> join('product', 'packet_product.product_id', '=', 'product.id')
+                    -> where('packet.id', '=', $packet_id)
+                    -> get();
+                foreach ($products as $product) {
+                    array_push($total_products, $product);
+                }
             } else {
                 return "Packet not found\n" . Packet::find($packet_id);
             }
         }
 
-        return 'true';
+        foreach ($total_products as $product) {
+            foreach ($goods as $good) {
+                if($product -> id == $good -> id) {
+                    if($good -> total_amount >= $product -> amount) {
+                        $good -> total_amount = $good -> total_amount - $product -> amount;
+                    } else {
+                        return "not enough goods at Store";
+                    }
+                }
+            }
+        }
+        foreach ($goods as $item) {
+            $good = Goods::find($item -> id);
+
+            $good -> total_amount = $item -> total_amount;
+
+            $good -> save();
+        }
+
+        $goodsAfter = Goods::all();
+
+        $newPayment = new Payments();
+
+        $newPayment -> data = implode(" ", $packet_id_array);
+
+        $newPayment -> status = 2; // Status wait 15 min
+
+        $newPayment -> description  =  "Some description";
+
+        $newPayment -> created_at = Carbon::now();
+        $newPayment -> updated_at = Carbon::now();
+
+        $newPayment -> save();
+
+
+        return "success";
     }
 }
