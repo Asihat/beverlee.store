8dcc325c2ae02bd688827e890734d1a35a6c503d
mukhan <mr@indigo24.com>
2020-01-17 17:47:51 +0600

    CheckData

--- app/Http/Controllers/API/CheckDataController.php
+++ app/Http/Controllers/API/CheckDataController.php
@@ -1,123 +1,5 @@
 <?php
 
-//namespace App\Http\Controllers\API;
-//
-//use App\Goods;
-//use App\Http\Controllers\Controller;
-//use App\Packet;
-//use App\Payments;
-//use App\Product;
-//use Carbon\Carbon;
-//use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\DB;
-//
-//class CheckDataController extends Controller {
-//    public function check(Request $request) {
-//
-//        $token = $request->token;
-//        $packet_id_array = $request->packet_id;
-//        $order_id = $request->input('order_id');
-//
-//        $goods = Goods::all();
-//        $total_products = [];
-//
-//        $payment = Payments::where('order_id', $order_id)->get();
-//        if (md5($packet_id_array.$order_id) == $token){
-//            if (count($payment) > 0) {
-//                return 'false';
-//            } else {
-//                foreach ($packet_id_array as $packet_id) {
-//                    if (Packet::find($packet_id)) {
-//                        $products = DB::table('packet')
-//                            ->join('packet_product', 'packet.id', '=', 'packet_product.packet_id')
-//                            ->join('product', 'packet_product.product_id', '=', 'product.id')
-//                            ->where('packet.id', '=', $packet_id)
-//                            ->get();
-//
-//                        foreach ($products as $product) {
-//                            array_push($total_products, $product);
-//                        }
-//                    } else {
-//                        return "Packet not found\n" . Packet::find($packet_id);
-//                    }
-//                }
-//
-//                foreach ($total_products as $product) {
-//                    foreach ($goods as $good) {
-//                        if ($product->id == $good->id) {
-//                            if ($good->total_amount >= $product->amount) {
-//                                $good->total_amount = $good->total_amount - $product->amount;
-//                            } else {
-//                                return "not enough goods at Store";
-//                            }
-//                        }
-//                    }
-//                }
-//                foreach ($goods as $item) {
-//                    $good = Goods::find($item->id);
-//
-//                    $good->total_amount = $item->total_amount;
-//
-//                    $good->save();
-//                }
-//
-//                $goodsAfter = Goods::all();
-//
-//                $newPayment = new Payments();
-//
-//                $newPayment->data = $packet_id_array;
-//                $newPayment->status = 1; // Status wait 15 min
-//                $description = description($packet_id_array);
-//                $newPayment->description = subArraysToString($description, ",");
-//
-//                $newPayment->created_at = Carbon::now();
-//                $newPayment->updated_at = Carbon::now();
-//                $newPayment->order_id = $order_id;
-//                $newPayment->save();
-//
-//                return 'success';
-//            }
-//
-//        } else {
-//            return 'incorrect token';
-//        }
-//    }
-//
-//
-//
-//
-////    }
-//    }
-//
-//    function subArraysToString($ar, $sep = ' ') {
-//        $str = '';
-//        foreach ($ar as $val) {
-//            $str .= implode($val);
-//            $str .= $sep; // add separator between sub-arrays
-//        }
-//        $str = rtrim($str, $sep); // remove last separator
-//        return $str;
-//    }
-//
-//    function description($packet_id_array) {
-//        $desc = explode(",", $packet_id_array);
-//
-//        $explode = array();
-//        for ($i = 0; $i < count($desc); $i++) {
-//            array_push($explode, explode("=>", $desc[$i]));
-//        }
-//
-//        $description = array();
-//        for ($j = 0; $j < count($explode); $j++) {
-//            $pro = Packet::find($explode[$j][0])->get();
-//            array_push($description, array($explode[$j][1]," штук ",$pro[$j]['name']));
-//        }
-////        array_pop($description);
-//        return $description;
-////    }
-//}
-
-
 namespace App\Http\Controllers\API;
 
 use App\Goods;
@@ -157,7 +39,7 @@ class CheckDataController extends Controller {
         $goods = Goods::all();
         $total_products = [];
 
-        $newToken = md5($packet_ids.$order_id);
+        $newToken = md5($packet_ids . $order_id);
         if ($newToken == $token) {
             $payment = Payments::where('order_id', $order_id)->get();
             if (count($payment) > 0) {
@@ -371,8 +253,4 @@ SQL;
 
         return $description;
     }
-
 }
-
-
-
